5c7b21c649d1530166dc3f11977d266d70557651
Amir Dhahri <dhahriamir@outlook.com>
2026-04-20 10:10:43 +0100

    Update

---
+++ models/Notification.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", // who triggered the notification
+            required: true,
+        },
+
+        receivers: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User", // who will receive it
+                required: true,
+            }
+        ],
+
+        type: {
+            type: String,
+            enum: ["PROJECT_COMPLETED", "MESSAGE", "SYSTEM"],
+            required: true,
+        },
+
+        entity: {
+            type: String, // e.g. "StageNet Platform"
+        },
+
+        entityType: {
+            type: String, // e.g. "Project"
+        },
+
+        message: {
+            type: String, // optional fallback text
+        },
+
+        isRead: {
+            type: Boolean,
+            default: false,
+        },
+
+        senderPhoto: {
+            type: String, // for quick display (optional optimization)
+        },
+    },
+    {
+        timestamps: true, // gives you createdAt (for "5 min ago")
+    }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
--- server.js
+++ server.js
@@ -1,14 +1,48 @@
 require("dotenv").config()
 const http = require("http")
-const app = require("./app/app")
+const app = require("./app/app");
+const cors = require("cors");
+const { Server } = require("socket.io");
 require("./config/dbConnect");
-const PORT = process.env.PORT || 2026; 
+const PORT = process.env.PORT || 2026;
 
 
 //=====Server=====//
 
 const server = http.createServer(app);
 
-server.listen(PORT,() => {
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000/",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", async (socket) => {
+    socket.on("connection", async (socket) => {
+        console.log("Client Connected!");
+    })
+    socket.on("sendNotification", async (data) => {
+        console.log(data);
+        try {
+            const notifications = [];
+            for (const receiver of data.receivers) {
+                const notif = await Notification.create({
+                    ...data,
+                    receiver
+                });
+                notifications.push(notif);
+                io.to(receiver.toString()).emit("receiveNotification", notif)
+            }
+        } catch (error) {
+            console.log("Error saving notification", error);
+        }
+    })
+    socket.on("disconnect", async (socket) => {
+        onsole.log("Client Disconnect!");
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
