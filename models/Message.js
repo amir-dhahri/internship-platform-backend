f2acc198f1d415507a75d6166ef18c452d5ee0da
Amir Dhahri <dhahriamir@outlook.com>
2026-05-02 15:59:31 +0100

    Update

---
+++ models/Message.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+    {
+        roomId: {
+            type: String,
+            required: true,
+            index: true, // for faster queries
+        },
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
+        receiverId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        attachments: [
+            {
+                url: String,
+                type: String,
+            },
+        ],
+        readBy: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
